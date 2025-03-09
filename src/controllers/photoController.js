585458d9bf83a0addff3fb50efc59ce87dd2a39e
TeDeCanela <153479319+TeDeCanela@users.noreply.github.com>
2025-03-08 20:57:31 -0600

    Test de Post, get y delete

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const Photo =require("../models/Photo");
 
 //Subir foto
@@ -29,12 +31,30 @@ exports.getUserPhotos = async (req, res) => {
 
 //Eliminar una foto
 exports.deletePhoto = async (req, res) => {
-    try{
-        const { id } = req.params;
+    try {
+      const { id } = req.params;
+  
+      const photo = await Photo.findById(id);
+      if (!photo) {
+        return res.status(404).json({ error: "Foto no encontrada" });
+      }
+      const isTestEnv = process.env.NODE_ENV === "test";
+      
+      const filePath = (isTestEnv)
+                  ? path.join(__dirname, "../../", photo.imageUrl)
+                  : path.join(__dirname, "../", photo.imageUrl);
+                  
+      fs.unlink(filePath, async (err) => {
+        if (err) {
+          console.error("Error al eliminar el archivo:", err);
+          return res.status(500).json({ error: "Error al eliminar la imagen del servidor" });
+        }
+  
         await Photo.findByIdAndDelete(id);
-
+  
         res.status(200).json({ message: "Foto eliminada con éxito" });
-    }catch (error){
-        res.status(500).json({ error:"Error al eliminar la foto" });
+      });
+    } catch (error) {
+      res.status(500).json({ error: "Error al eliminar la foto" });
     }
 };
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
-require ("dotenv").config({path: './variables_entorno.env'});
+require("dotenv").config({ path: require("path").resolve(__dirname, './variables_entorno.env') });
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));//Servir imágenes estáticamente
@@ -20,5 +21,9 @@ mongoose
 const photoRoutes = require("./routes/photoRoutes");
 app.use("/api/photos", photoRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+module.exports = app; 
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+}
